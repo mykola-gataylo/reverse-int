516de1379056220185dc48e921daf7a9348a3a87
Mykola Gataylo <mykola.gataylo@gmail.com>
2021-03-13 06:14:58 +0100

    fix: fix errores

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+
+    return n = +n.toString().split('').reverse().join('');
 }
